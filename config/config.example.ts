b5f3853f5f21d5f5aa2cabc67ac1ee0709096308
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-05-23 22:46:54 +0200

    Init commit.

---
+++ config/config.example.ts
@@ -0,0 +1,8 @@
+// export const config = {
+//     dbHost: 'localhost',
+//     dbUser: 'user',
+//     dbPassword: 'password',
+//     dbDatabase: 'database',
+//     dbPort: 8889,
+//     corsOrigin: 'http//localhost:3000',
+// };
