e31ce783e18100ad03d03d1789d90fda01eceb5d
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-05-29 11:11:00 +0200

    EP-s were added to shopRouter.

--- routers/shop.router.ts
+++ routers/shop.router.ts
@@ -1,10 +1,26 @@
 import {Router} from "express";
+import {ShopRecord} from "../records/shop.record";
 
 export const shopRouter = Router();
 
 shopRouter
-    .get('/', (req, res) => {
-        res.json({
-            ok: 'true',
-        });
+    .get('/', async (req, res) => {
+        const shops = await ShopRecord.getAll();
+        console.log(shops);
+        res.json(shops);
+    })
+    .get('/edit/:id', async (req, res) => {
+        const shopToEdit = await ShopRecord.getOne(req.params.id);
+        res.json(shopToEdit);
+    })
+    .delete('/delete/:id', async (req, res) => {
+        const shopToDelete = await ShopRecord.getOne(req.params.id);
+        const shopName = shopToDelete.name;
+        await shopToDelete.delete();
+        res.json(shopName);
+    })
+    .post('/add', async (req, res) => {
+        const newShop = new ShopRecord(req.body);
+        await newShop.insert();
+        res.json(newShop);
     });
