9656e5a4807e98ebd822b1aa36665b93ecd94b3f
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-06-08 21:22:16 +0200

    EP to add product was added.

--- routers/product.router.ts
+++ routers/product.router.ts
@@ -11,4 +11,9 @@ productRouter
     .get('/singleProduct/:shopId/:productName', async (req, res) => {
         const oneProductFromSingleShop = await ProductRecord.getOneProductFromSingleShop(req.params.shopId, req.params.productName);
         res.json(oneProductFromSingleShop);
+    })
+    .post('/add', async (req, res) => {
+        const newProduct = new ProductRecord(req.body);
+        await newProduct.insert();
+        res.json(newProduct);
     });
