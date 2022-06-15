67c1f52724eb499703a59718b1a6131436107a71
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-06-15 14:44:10 +0200

    db.ts was fixed.

--- utils/db.ts
+++ utils/db.ts
@@ -4,7 +4,7 @@ import {config} from "../config/config";
 export const pool = createPool({
     host: config.dbHost,
     user: config.dbUser,
-    password: config.dbUser,
+    password: config.dbPassword,
     port: config.dbPort,
     database: config.dbDatabase,
     namedPlaceholders: true,
